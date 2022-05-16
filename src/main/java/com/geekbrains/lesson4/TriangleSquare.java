4c2e72439905eab52cd3c9b74b20c46a2a6ca43e
madmax52rus <88784403+madmax52rus@users.noreply.github.com>
2022-05-16 08:08:52 +0300

    Update TriangleSquare.java

--- src/main/java/com/geekbrains/lesson4/TriangleSquare.java
+++ src/main/java/com/geekbrains/lesson4/TriangleSquare.java
@@ -1,11 +1,11 @@
 package com.geekbrains.lesson4;
 
-import java.util.Scanner;
-
 public class TriangleSquare {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+    public static boolean isTriangleSquare(int a, int b, int c) {
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println(" Сумма двух сторон НЕ должна быть равна третьей стороне!");
         System.out.println("Enter side A: ");
         int a = scanner.nextInt();
             if(a <= 0) {
@@ -23,9 +23,12 @@ public class TriangleSquare {
             if(c <= 0) {
                 System.out.println("Invalid value!!!");
                 System.exit(1);
-            }
+            }*/
+
+        if (a <= 0 || b <= 0 || c <= 0) return false;
         double pp = (a + b + c) / 2.0; // HalfPerimeter
-        double triangleSquare = Math.sqrt(pp * (pp - a) * (pp - b) * (pp -c));
+        double triangleSquare = Math.sqrt(pp * (pp - a) * (pp - b) * (pp - c));
         System.out.println("Площадь треугольника = " + triangleSquare);
+        return true;
     }
 }
